9b4bf81b220971bd3e1368f64ba26feaff7a2601
vakr <vakr@microsoft.com>
2022-04-21 16:47:44 -0700

    returning a struct

--- test/ebpf/testSockProg.c
+++ test/ebpf/testSockProg.c
@@ -4,50 +4,58 @@
 #include "bpf/libbpf.h"
 
 typedef struct bpf_object go_bpf_obj;
-int test_ebpf_prog()
+npm_endpoint_prog_t test_ebpf_prog()
 {
-    FILE *ptr;
-    char ch;
-
-    // Opening file in reading mode
-    ptr = fopen("cgroup_sock_addr.o", "r");
-
-    if (NULL == ptr)
-    {
-        printf("file can't be opened \n");
-    }
-
-    printf("content of this file are \n");
-
-    // Printing what is written in file
-    // character by character using loop.
-    do
-    {
-        ch = fgetc(ptr);
-        printf("%c", ch);
-
-        // Checking if character is not EOF.
-        // If it is EOF stop eading.
-    } while (ch != EOF);
-
-    // Closing the file
-    fclose(ptr);
-
+    struct npm_endpoint_prog_t _npm_endpoint_prog_t;
     struct bpf_object *object;
     int program_fd;
     int result = bpf_prog_load("cgroup_sock_addr.o", BPF_PROG_TYPE_CGROUP_SOCK_ADDR, &object, &program_fd);
 
     if (!object)
     {
-        printf("object is not null\n");
+        printf("object is null\n");
+        return NULL;
     }
-    else
-    {
-        printf("Loaded program\n");
-        printf("%d program fd \n", program_fd);
-    }
-
+    printf("Loaded program\n");
+    printf("%d program fd \n", program_fd);
     printf("%d result \n", result);
 
-    return result;
+    printf("Getting the bpf_prog for connect program\n");
+    bpf_program *connect_program = bpf_object__find_program_by_name(object, connect4_program_name);
+    if (!connect_program)
+    {
+        printf("%s is null\n", connect4_program_name);
+        return NULL;
+    }
+
+    _npm_endpoint_prog_t.connect4_program = connect_program;
+
+    bpf_program *connect6_program = bpf_object__find_program_by_name(object, connect6_program_name);
+    if (!connect6_program)
+    {
+        printf("%s is null\n", connect6_program_name);
+        return NULL;
+    }
+
+    _npm_endpoint_prog_t.connect6_program = connect6_program;
+
+    bpf_program *recv_accept_program = bpf_object__find_program_by_name(object, recv4_accept_program_name);
+    if (!recv_accept_program)
+    {
+        printf("%s is null\n", recv4_accept_program_name);
+        return NULL;
+    }
+
+    _npm_endpoint_prog_t.recv4_accept_program = recv_accept_program;
+
+    bpf_program *recv6_accept_program = bpf_object__find_program_by_name(object, recv6_accept_program_name);
+    if (!recv6_accept_program)
+    {
+        printf("%s is null\n", recv6_accept_program_name);
+        return NULL;
+    }
+
+    _npm_endpoint_prog_t.recv4_accept_program = recv6_accept_program;
+
+    return _npm_endpoint_prog_t;
 }
--- test/ebpf/testSockProg.h
+++ test/ebpf/testSockProg.h
@@ -1,1 +1,14 @@
 int test_ebpf_prog();
+
+struct npm_endpoint_prog_t
+{
+    bpf_program *connect4_program;
+    bpf_program *connect6_program;
+    bpf_program *recv4_accept_program;
+    bpf_program *recv6_accept_program;
+};
+
+const char *connect4_program_name = "authorize_connect4";
+const char *connect6_program_name = "authorize_connect6";
+const char *recv4_accept_program_name = "authorize_recv_accept4";
+const char *recv6_accept_program_name = "authorize_recv_accept6";
