2652039a9b3345ac7f7285740bf080bfa3fe494e
vakalapa <vakr@microsoft.com>
2022-05-05 16:46:02 +0000

    adding the right supported bpf trace printk

--- test/ebpf/src/endpoint_prog.c
+++ test/ebpf/src/endpoint_prog.c
@@ -48,20 +48,6 @@ struct bpf_map_def ip_cache_map = {
 // TODO declare identity cache map
 
 
-__inline void
-print_trace(bpf_sock_addr_t *ctx, char *msg)
-{
-    /*
-    char packet_info[256];
-    bpf_snprintf(packet_info, sizeof(packet_info),  "Packet info: Src IP: %x, Dst IP: %x, Src Port: %d, Dst Port: %d, Compartment ID: %d\n", ctx->msg_src_ip4, ctx->user_ip4, ctx->user_port, ctx->user_port, ctx->compartment_id);
-
-    char print_msg[1024];
-    bpf_snprintf(print_msg, sizeof(print_msg),  "%s. %s\n", msg ,packet_info);
-    bpf_trace_printk(print_msg, sizeof(print_msg));
-    */
-    bpf_trace_printk(msg, sizeof(msg));
-}
-
 __inline int
 _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key)
 {
@@ -69,7 +55,7 @@ _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key
     void *policy_map_fd = (int32_t *)bpf_map_lookup_elem(&map_policy_maps, &compartment_id);
     if (policy_map_fd == NULL)
     {
-        print_trace(ctx, "Policy Eval: No policy map for compartment");
+        bpf_printk("Policy Eval: No policy map for compartment");
         // if there is no policy map attached to this compartment
         // then no policy is applied, allow all traffic.
         return CGROUP_ACT_OK;
@@ -79,9 +65,9 @@ _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key
     verdict = bpf_map_lookup_elem(policy_map_fd, &key);
     if (verdict != NULL)
     {
-        char msg[128];
-        bpf_snprintf(msg, sizeof(msg), "Policy Eval: L4 policy ID %lu Allowed.", (unsigned long *) verdict, sizeof(verdict));
-        print_trace(ctx, msg);
+        //char msg[128];
+        bpf_printk("Policy Eval: L4 policy ID %lu Allowed.", *(unsigned long *) verdict);
+        //bpf_printk(msg);
         return CGROUP_ACT_OK;
     }
 
@@ -90,9 +76,9 @@ _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key
     verdict = bpf_map_lookup_elem(policy_map_fd, &key);
     if (verdict != NULL)
     {
-        char msg[128];
-        bpf_snprintf(msg, sizeof(msg), "Policy Eval: L3 policy ID %lu Allowed.", (unsigned long *) verdict, sizeof(verdict));
-        print_trace(ctx, msg);
+        //char msg[128];
+        bpf_printk("Policy Eval: L3 policy ID %lu Allowed.", *(unsigned long *) verdict);
+        //bpf_printk(msg);
         return CGROUP_ACT_OK;
     }
 
@@ -113,7 +99,7 @@ authorize_v4(bpf_sock_addr_t *ctx, direction_t dir)
     ctx_label_id = (uint32_t *)bpf_map_lookup_elem(&ip_cache_map, &ip_to_lookup);
     if (ctx_label_id == NULL)
     {
-        print_trace(ctx, "No label found for IP");
+        bpf_printk("No label found for IP");
         // if there is no Identity assigned then CP is yet to sync
         // allow all traffic.
         return CGROUP_ACT_OK;
@@ -142,7 +128,7 @@ authorize_v6(bpf_sock_addr_t *ctx, direction_t dir)
     ctx_label_id = (uint32_t *)bpf_map_lookup_elem(&ip_cache_map, &ip_to_lookup);
     if (ctx_label_id == NULL)
     {
-        print_trace(ctx, "No label found for IP");
+        bpf_printk("No label found for IP");
         // if there is no Identity assigned then CP is yet to sync
         // allow all traffic.
         return CGROUP_ACT_OK;
@@ -160,27 +146,27 @@ authorize_v6(bpf_sock_addr_t *ctx, direction_t dir)
 SEC("cgroup/connect4")
 int authorize_connect4(bpf_sock_addr_t *ctx)
 {
-    print_trace(ctx, "Connect4 called.");
+    bpf_printk("Connect4 called.");
     return authorize_v4(ctx, EGRESS);
 }
 
 SEC("cgroup/connect6")
 int authorize_connect6(bpf_sock_addr_t *ctx)
 {
-    print_trace(ctx, "Connect6 called.");
+    bpf_printk("Connect6 called.");
     return authorize_v6(ctx, EGRESS);
 }
 
 SEC("cgroup/recv_accept4")
 int authorize_recv_accept4(bpf_sock_addr_t *ctx)
 {
-    print_trace(ctx, "Recv_accept4 called.");
+    bpf_printk("Recv_accept4 called.");
     return authorize_v4(ctx, INGRESS);
 }
 
 SEC("cgroup/recv_accept6")
 int authorize_recv_accept6(bpf_sock_addr_t *ctx)
 {
-    print_trace(ctx, "Recv_accept6 called.");
+    bpf_printk("Recv_accept6 called.");
     return authorize_v6(ctx, INGRESS);
 }
