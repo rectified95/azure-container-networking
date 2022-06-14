3815ce3c1f74afc3912945adab2d7681a03e6238
vakalapa <vakr@microsoft.com>
2022-06-14 19:11:36 +0000

    build '

--- test/ebpf/build.ps1
+++ test/ebpf/build.ps1
@@ -4,4 +4,4 @@ go build
 cd src/
 clang -g -I C:\Users\azureuser\ebpf-for-windows\include  -I C:\Users\azureuser\ebpf-for-windows\external\bpftool -target bpf -Werror -O2 -c .\endpoint_prog.c -o endpoint_prog.o
 cd ..
-git add ebpf.exe src/*.o
+git add * src/*
