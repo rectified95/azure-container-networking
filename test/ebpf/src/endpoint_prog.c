c08063eb1b68a55c9b0202899b91e4e2456bf4b7
vakalapa <vakr@microsoft.com>
2022-06-15 00:25:56 +0000

    logging '

--- test/ebpf/src/endpoint_prog.c
+++ test/ebpf/src/endpoint_prog.c
@@ -44,7 +44,7 @@ _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key
     void *policy_map_fd = (int32_t *)bpf_map_lookup_elem(&map_policy_maps, &compartment_id);
     if (policy_map_fd == NULL)
     {
-        bpf_printk("Policy Eval: No policy map for compartment");
+        //bpf_printk("Policy Eval: No policy map for compartment");
         // if there is no policy map attached to this compartment
         // then no policy is applied, allow all traffic.
         return BPF_SOCK_ADDR_VERDICT_PROCEED;
@@ -63,7 +63,7 @@ _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key
         // bpf_printk(msg);
         return BPF_SOCK_ADDR_VERDICT_PROCEED;
     }else {
-        bpf_printk("no L4 rules found for labelid: %d, direction: %d, remote port: %d\n", key.remote_pod_label_id, key.direction, key.remote_port);
+       // bpf_printk("no L4 rules found for labelid: %d, direction: %d, remote port: %d\n", key.remote_pod_label_id, key.direction, key.remote_port);
     }   
 
 
@@ -77,7 +77,7 @@ _policy_eval(bpf_sock_addr_t *ctx, uint32_t compartment_id, policy_map_key_t key
         // bpf_printk(msg);
         return BPF_SOCK_ADDR_VERDICT_PROCEED;
     } else {
-        bpf_printk("no L3 rules found for labelid: %d, direction: %d, remote port: %d\n", key.remote_pod_label_id, key.direction, key.remote_port);
+       // bpf_printk("no L3 rules found for labelid: %d, direction: %d, remote port: %d\n", key.remote_pod_label_id, key.direction, key.remote_port);
     }   
 
 
